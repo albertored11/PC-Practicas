264e6f5f730bbb215514d73c00ff0e6b2deeecc0
Alberto Redondo <albertomost@gmail.com>
2021-05-27 18:23:24 +0200

    Práctica 5: añadidos algunos comentarios; error cuando el fichero que se pide no lo tiene nadie; faltaba nuevo mensaje

---
+++ Práctica 5/PC-P5-2/src/com/p5_2/MensajeNoExisteFichero.java
@@ -0,0 +1,18 @@
+package com.p5_2;
+
+public class MensajeNoExisteFichero extends Mensaje {
+
+    private String _filename;
+
+    public MensajeNoExisteFichero(String filename) {
+
+        super("MENSAJE_NO_EXISTE_FICHERO");
+
+        _filename = filename;
+
+    }
+
+    public String getFilename() {
+        return _filename;
+    }
+}
